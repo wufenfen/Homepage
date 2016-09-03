6b9620cc2e7361160318521fe7608c4b259fa182
wufenfen <747042796@qq.com>
2016-09-03 21:40:35 +0800

    misc: finish the portfolio part

--- src/main/prototype/scripts/main.js
+++ src/main/prototype/scripts/main.js
@@ -1,70 +1,85 @@
-$(document).ready(function(){ 
-	var cover = $('#cover'); 
-	var content = ['cover', 'about','services','portfolio','skills','resume','testimonials','contact'];
-	var $content = [];
-	$(content).each(function(i,item){ 
-		$content[i] = $('#' + item);
-	})
+$(document).ready(function() {
+    var cover = $('#cover');
+    var content = ['cover', 'about', 'services', 'portfolio', 'skills', 'resume', 'testimonials', 'contact'];
+    var $content = [];
+    $(content).each(function(i, item) {
+        $content[i] = $('#' + item);
+    });
 
-	var navBar = $('#nav');  
-	var navBarHeight = navBar.height();
+    var navBar = $('#nav');
+    var navBarHeight = navBar.height();
 
- 	function fixNavBar(){
- 		//fixed the bug when navbar change from static to fixed
- 		var height = navBar.css('position') == 'fixed'? 0: navBarHeight;
- 		if( $(window).scrollTop() > cover.height() + height){
-			navBar.addClass('navFixed');
-			$('#blank').css('display','block');//make the about region the same value
-		}
-		else{
-			navBar.removeClass('navFixed');
-			$('#blank').css('display','none');
-		}
- 	}
+    function fixNavBar() {
+        //fixed the bug when navbar change from static to fixed
+        var height = navBar.css('position') == 'fixed' ? 0 : navBarHeight;
+        if ($(window).scrollTop() > cover.height() + height) {
+            navBar.addClass('navFixed');
+            $('#blank').css('display', 'block'); //make the about region the same value
+        } else {
+            navBar.removeClass('navFixed');
+            $('#blank').css('display', 'none');
+        }
+    }
 
-	function isHighlight(start, end, target){
-		var select = 'a[href="#'+ target  + '"]';
-		//hightlight only when the element is static
-		var isAnimating = $("html, body").is(':animated'); 
-		if(isAnimating){
-			$(select).removeClass('hover'); 
-			return;
-		}
+    function isHighlight(start, end, target) {
+        var select = 'a[href="#' + target + '"]';
+        //hightlight only when the element is static
+        var isAnimating = $("html, body").is(':animated');
+        if (isAnimating) {
+            $(select).removeClass('hover');
+            return;
+        }
 
-		if( $(window).scrollTop() >= start.offset().top + start.height() && $(window).scrollTop() < end.offset().top + end.height()){
-			$(select).addClass('hover');
-		}
-		else{
-			$(select).removeClass('hover');
-		}
-	}
+        if ($(window).scrollTop() >= start.offset().top + start.height() && $(window).scrollTop() < end.offset().top + end.height()) {
+            $(select).addClass('hover');
+        } else {
+            $(select).removeClass('hover');
+        }
+    }
 
-	function highlightNar(){ 
-		for (var i = 0; i < $content.length-1; i++) { 
-			isHighlight($content[i], $content[i+1], content[i+1]);
-		};  
-	}
+    function highlightNar() {
+        for (var i = 0; i < $content.length - 1; i++) {
+            isHighlight($content[i], $content[i + 1], content[i + 1]);
+        };
+    }
 
 
+    fixNavBar();
 
- 	fixNavBar();
+    $(window).scroll(function() {
+        fixNavBar();
+        highlightNar();
+    })
 
-	$(window).scroll(function(){
-		fixNavBar();
-		highlightNar();
-	})
+    $('.m-nav, .m-cover').click(function(e) {
+        var href = e.target.href;
+        if (href) {
+            var id = href.split('#')[1];
+            var target = $('#' + id);
+            console.log(target.offset().top - navBarHeight);
+            $('html, body').stop().animate({
+                'scrollTop': target.offset().top - navBarHeight
+            }, 500, 'linear');
+        }
+        highlightNar();
+    })
 
-	$('.m-nav, .m-cover').click(function(e){
-		var href = e.target.href;
-		if( href){
-			var id= href.split('#')[1];
-			var target = $('#' + id); 
-			console.log(target.offset().top - navBarHeight);
-			$('html, body').stop().animate({
-		        'scrollTop': target.offset().top - navBarHeight
-		    }, 500, 'linear');
-		} 
-		highlightNar();
-	})
- 
+
+    // init Isotope
+    var $Pics = $('.m-portPics').isotope({
+        transitionDuration: '0.8s',
+        social_tools:''
+    });
+    // filter items on click
+    $('.j-filter-group').click (function(e) {
+        $('.j-filter-group .hover').removeClass('hover');
+
+        var filterValue = $(e.target).attr('data-filter');
+        $Pics.isotope({ filter: filterValue });
+        $(e.target).addClass('hover');
+    });
+
+
+    $(".u-images a[rel^='prettyPhoto']").prettyPhoto({ animation_speed: 'normal', theme: 'light_square', slideshow: 3000, autoplay_slideshow: true });
+
 })
